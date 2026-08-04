dfd5baf0fda1a91aa15fbeb6d93eab06bd1da92c
Administrator <sibusiso.nkwanyana@simplify.africa>
2026-08-04 20:06:15 +0200

    added method test to login

--- basic/test_login.py
+++ basic/test_login.py
@@ -1,5 +1,9 @@
+from telnetlib import EC
+
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class TestLogin:
@@ -9,6 +13,8 @@ class TestLogin:
     username_id = "login-email"
     password_id = "login-password"
     login_button_id = "login-submit"
+    verify_dashboard_content_xpath = "//h2"
+
 
     def test_login(self):
         #start the browser and navigate to the website
@@ -16,3 +22,9 @@ class TestLogin:
         self.driver.get(self.website_url)
 
         wait = WebDriverWait(self.driver, 10)
+        wait.until(EC.visibility_of_element_located((By.XPATH, self.main_login_button_xpath))).click()
+        self.driver.find_element(By.ID, self.username_id).send_keys("nkwanyana@gmail.com")
+        self.driver.find_element(By.ID, self.password_id).send_keys("#12345678")
+        self.driver.find_element(By.ID, self.login_button_id).click()
+        wait.until(EC.visibility_of_element_located((By.XPATH, self.verify_dashboard_content_xpath))).is_displayed()
+
