22c0ce9e74d204fed1e3911702662f7930128596
Dilz3 <ndfihlela@gmail.com>
2026-08-10 23:31:45 +0200

    Update to the login_page

--- pageObjects/login_page.py
+++ pageObjects/login_page.py
@@ -1,3 +1,4 @@
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 
 class LoginPage:
@@ -10,11 +11,11 @@ class LoginPage:
         self.driver = driver
 
     def getUsername(self):
-        wait = WebDriverWait(self.driver, 10)
-        wait.until(self.driver.find_element_by_xpath(self.username_id)).send_keys(self.username_id)
+            wait = WebDriverWait(self.driver, 10)
+            wait.until(self.driver.find_element(By.ID, self.username_id).send_keys("noxolo.demo@gmail.com"))
 
     def getPassword(self):
-        self.driver.find_element_by_xpath(self.password_id).send_keys(self.password_id)
+            self.driver.find_element(By.ID, self.password_id).send_keys("password9%")
 
     def getLoginButton(self):
-        self.driver.find_element_by_xpath(self.login_button_id).click()
+            self.driver.find_element(By.ID, self.login_button_id).click()
