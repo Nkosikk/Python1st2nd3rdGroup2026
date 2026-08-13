2e26208e318c16cebc0c0c96c05ba89dd99b703f
Administrator <sibusiso.nkwanyana@simplify.africa>
2026-08-13 20:08:29 +0200

    refactored code in test_login class by calling reusable methods

--- tests/test_login.py
+++ tests/test_login.py
@@ -5,9 +5,13 @@ import pytest
 
 from pageObjects.home_page import Home_page
 from pageObjects.login_page import LoginPage
+from utils.LoginFunction import login
 from utils.config_properties import ReadConfig_CommonDetails
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
+from pageObjects.dashboard_page import dashboard_page
+
+from utils.launchBrowser import launch_browser
 
 
 class TestLogin:
@@ -20,31 +24,18 @@ class TestLogin:
 
     @pytest.mark.sanity
     def test_valid_login(self, setup):
-        self.driver = setup
-        self.driver.get(self.dev_url)
 
-        home = Home_page(self.driver)
-        home.click_main_login_button()
-
-        login = LoginPage(self.driver)
-        login.getUsername(self.username)
-        login.getPassword(self.password)
-        login.clickLoginButton()
+        self.driver = launch_browser(setup)
+        login(self.driver, self.username, self.password)
+        dashboard = dashboard_page(self.driver)
+        dashboard.verify_dashboard_page()
         sleep(10)
         allure.attach(self.driver.get_screenshot_as_png(), name="Login Positive", attachment_type=allure.attachment_type.PNG)
 
-    @pytest.mark.test
+    @pytest.mark.sanity
     def test_invalid_login(self, setup):
-        self.driver = setup
-        self.driver.get(self.dev_url)
-
-        home = Home_page(self.driver)
-        home.click_main_login_button()
-
-        login = LoginPage(self.driver)
-        login.getUsername(self.invalid_username)
-        login.getPassword(self.invalid_password)
-        login.clickLoginButton()
+        self.driver = launch_browser(setup)
+        login(self.driver, self.invalid_username, self.invalid_password)
         wait = WebDriverWait(self.driver, 10)
         alert = wait.until(EC.alert_is_present())
         alert.accept()
