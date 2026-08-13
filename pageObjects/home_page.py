01a4db22ee5923cbdacccf92884ab868d9ddac53
Administrator <sibusiso.nkwanyana@simplify.africa>
2026-08-13 18:40:19 +0200

    added runProjectOnWindows.bat refactered code in home_page and login_page classes

--- pageObjects/home_page.py
+++ pageObjects/home_page.py
@@ -1,5 +1,7 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
+
 
 
 class Home_page:
@@ -11,4 +13,4 @@ class Home_page:
 
     def click_main_login_button(self):
         wait = WebDriverWait(self.driver, 10)
-        wait.until(self.driver.find_element(By.XPATH,self.main_login_button_xpath).click())
+        wait.until(EC.visibility_of_element_located((By.XPATH,self.main_login_button_xpath))).click()
--- pageObjects/login_page.py
+++ pageObjects/login_page.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions
 
 class LoginPage:
 
@@ -12,7 +13,7 @@ class LoginPage:
 
     def getUsername(self, username ):
         wait = WebDriverWait(self.driver, 10)
-        wait.until(self.driver.find_element(By.ID ,self.username_id)).send_keys(username)
+        wait.until(expected_conditions.visibility_of_element_located((By.ID ,self.username_id))).send_keys(username)
 
     def getPassword(self, password ):
         self.driver.find_element(By.ID,self.password_id).send_keys(password)
