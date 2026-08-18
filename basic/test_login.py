c4f429908f1c12dbc5badf950abcbae022e81c64
Administrator <sibusiso.nkwanyana@simplify.africa>
2026-08-18 20:59:57 +0200

    added chrome options and set_window_size() method to basic test

--- basic/test_login.py
+++ basic/test_login.py
@@ -2,6 +2,7 @@ from telnetlib import EC
 
 import allure
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -19,8 +20,14 @@ class TestLogin:
 
     def test_login(self):
         #start the browser and navigate to the website
-        self.driver = webdriver.Chrome()
+        options = Options()
+        options.add_argument("--headless=new")  # modern headless mode
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--disable-gpu")  # Additional flag for CI stability
+        self.driver = webdriver.Chrome(options)
         self.driver.get(self.website_url)
+        self.driver.set_window_size(1920, 1080)
 
         wait = WebDriverWait(self.driver, 10)
         wait.until(EC.visibility_of_element_located((By.XPATH, self.main_login_button_xpath))).click()
